6d6c762d2aa43f643dc9071a34ff177283f84041
Angus Gaukroger <agaukroger77@gmail.com>
2022-09-22 16:23:21 +0100

    Add project files.

---
+++ BootstrapFinesse/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BootstrapFinesse.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
